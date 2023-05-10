dc881203b2ece277916af1b1a45b6f64f22ddec1
Aladdin <ninjalover249@gmail.com>
2023-05-10 18:33:52 -0300

    Borrar codigo viejo

--- app/src/main/java/com/example/loginarchivos/request/ApiClient.java
+++ app/src/main/java/com/example/loginarchivos/request/ApiClient.java
@@ -16,15 +16,7 @@ import java.io.ObjectOutputStream;
 
 public class ApiClient {
 
-//    private static SharedPreferences sp;
-
     private static File archivo;
-//    private static SharedPreferences conectar(Context context) {
-//        if (sp == null) {
-//            sp = context.getSharedPreferences("datos.xml", 0);
-//        }
-//        return sp;
-//    }
 
     private static File conectar (File dir){
         if(archivo == null){
@@ -32,17 +24,6 @@ public class ApiClient {
         }
         return archivo;
     }
-
-//    public static void guardar(Context context, Usuario usuario) {
-//        SharedPreferences sp = conectar(context);
-//        SharedPreferences.Editor editor = sp.edit();
-//        editor.putLong("dni", usuario.getDni());
-//        editor.putString("apellido", usuario.getApellido());
-//        editor.putString("nombre", usuario.getNombre());
-//        editor.putString("mail", usuario.getMail());
-//        editor.putString("pass", usuario.getPass());
-//        editor.commit();
-//    }
 
     public static void guardar(Context context, Usuario usuario){
         File archivo = conectar(context.getFilesDir());
@@ -56,17 +37,6 @@ public class ApiClient {
             e.printStackTrace();
         }
     }
-//
-//    public static Usuario leer(Context context) {
-//        SharedPreferences sp = conectar(context);
-//        Usuario usuario = new Usuario();
-//        usuario.setDni(sp.getLong("dni", -1));
-//        usuario.setApellido(sp.getString("apellido", "-1"));
-//        usuario.setNombre(sp.getString("nombre", "-1"));
-//        usuario.setMail(sp.getString("mail", "-1"));
-//        usuario.setPass(sp.getString("pass", "-1"));
-//        return usuario;
-//    }
 
     public static Usuario leer(Context context){
         File archivo = conectar(context.getFilesDir());
@@ -82,23 +52,6 @@ public class ApiClient {
             return null;
         }
     }
-
-//    public static Usuario login(Context context, String mail, String pass) {
-//        Usuario usuario = null;
-//        SharedPreferences sp = conectar(context);
-//        String email = sp.getString("mail", "-1");
-//        String passw = sp.getString("pass", "-1");
-//
-//        if (!email.equals(mail) || !passw.equals(pass)) {
-//            return usuario;
-//        }
-//
-//        Long dni = sp.getLong("dni", -1);
-//        String apellido = sp.getString("apellido", "-1");
-//        String nombre = sp.getString("nombre", "-1");
-//        usuario = new Usuario(dni, apellido, nombre, mail, pass);
-//        return usuario;
-//    }
 
     public static Usuario login(Context context, String mail, String pass){
         File archivo = conectar(context.getFilesDir());
